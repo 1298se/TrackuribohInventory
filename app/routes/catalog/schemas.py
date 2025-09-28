0e2767f995d55a3e19f6a7fc0bb8ac7601c4a4b7
Kayla Lin <klin23@u.rochester.edu>
2025-09-27 21:57:24 -0400

    fix

--- app/routes/catalog/schemas.py
+++ app/routes/catalog/schemas.py
@@ -123,6 +123,14 @@ class ProductTypesResponseSchema(BaseModel):
 
 class SetsResponseSchema(BaseModel):
     sets: list[SetBaseResponseSchema]
+    
+class TopPricedCardSchema(BaseModel):
+    sku_id: uuid.UUID
+    product_name: str
+    condition: str
+    printing: str
+    language: str
+    price: float
 
 class HistoricalPriceComparisonSchema(BaseModel):
     current_total_market_value: float
