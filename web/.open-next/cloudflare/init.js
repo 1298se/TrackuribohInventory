3a02b9d61a11fb5193f5409c679cef8d2b47d4dc
Kayla Lin <klin23@u.rochester.edu>
2025-10-11 22:28:23 -0400

    fix: cors

--- web/.open-next/cloudflare/init.js
+++ web/.open-next/cloudflare/init.js
@@ -49,7 +49,7 @@ function initRuntime() {
   };
   Object.assign(globalThis, {
     Request: CustomRequest,
-    __BUILD_TIMESTAMP_MS__: 1760225398169,
+    __BUILD_TIMESTAMP_MS__: 1760225752003,
     __NEXT_BASE_PATH__: "",
     __ASSETS_RUN_WORKER_FIRST__: false,
     __TRAILING_SLASH__: false,
