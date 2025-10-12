4e3bb77cba89cc74ab1d46886b1e366238bd84a9
Kayla Lin <klin23@u.rochester.edu>
2025-10-12 01:53:44 -0400

    remove workflow

--- web/api/fetcher.ts
+++ web/api/fetcher.ts
@@ -1,2 +1,5 @@
 export const API_URL =
-  process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:8000";
+  process.env.NEXT_PUBLIC_API_BASE_URL ||
+  (process.env.NODE_ENV === "production"
+    ? "https://trackuribohinventory-fragrant-bird-5215.fly.dev"
+    : "http://localhost:8000");
